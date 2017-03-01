7ea7cc5611465cb35d0508d030bcec961258159d
Hyperlazydude <AnimeAzn101@gmail.com>
2017-02-28 17:33:18 -0800

    Add shooting ability

---
+++ Assets/scripts/ShootAbility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAbility : MonoBehaviour {
+	public Bullet bullet;
+	public float speed;
+	public float timeDelete;
+	public float timeBetweenShots;
+	public Transform point;
+
+	private float current;
+
+	// Use this for initialization
+	void Awake () {
+		current = timeBetweenShots;
+		timeBetweenShots = 0;
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			timeBetweenShots -= Time.deltaTime;
+			if (timeBetweenShots <= 0) {
+				timeBetweenShots = current;
+				Bullet newBullet = Instantiate (bullet, point.position, point.rotation) as Bullet;
+				newBullet.speed = speed;
+				Destroy (newBullet.gameObject, timeDelete);
+			}
+		} else {
+			timeBetweenShots -= Time.deltaTime;
+		}
+	}
+}
