3e79b5c960c31ea432b47f253f279b6c77acbce5
Hyperlazydude <AnimeAzn101@gmail.com>
2017-02-28 23:02:33 -0800

    Added moving platform

---
+++ Assets/scripts/PlatformMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+	public Vector3 dest;
+	public float movementSpeed;
+
+	private Vector3 current;
+	private Vector3 move;
+	// Use this for initialization
+	void Awake () {
+		current = transform.position;
+		move = dest;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, move, movementSpeed * Time.deltaTime);
+		if (transform.position == move) {
+			move = current;
+		}
+		if (transform.position == current) {
+			move = dest;
+		}
+	}
+}
