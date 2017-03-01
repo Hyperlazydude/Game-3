fd41114db5fed6f0315075034b74db093f5defb0
kanirolf <wdomingo@uci.edu>
2017-02-28 19:16:04 -0800

    Fixed jumping to avoid "super-jumping".

--- Assets/scripts/DoubleJumpController.cs
+++ Assets/scripts/DoubleJumpController.cs
@@ -1,34 +1,9 @@
 ﻿using UnityEngine;
 
-public class DoubleJumpController : MonoBehaviour
+public class DoubleJumpController : JumpController
 {
-
-    public string jumpButtonName;
-    public float jumpForce;
-
-    private Rigidbody2D playerRB;
-
-    private int numJumps;
-
-    protected virtual void Awake()
+    protected override int AllowedJumps
     {
-        this.playerRB = this.GetComponent<Rigidbody2D>();
-
-        this.numJumps = 0;
-    }
-
-    protected virtual void FixedUpdate()
-    {
-        if (Input.GetButtonDown(this.jumpButtonName) && this.numJumps < 2)
-        {
-            this.numJumps++;
-            this.playerRB.AddForce(Vector2.up * this.jumpForce * Time.deltaTime);
-        }
-    }
-
-    protected virtual void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Platform") || collision.CompareTag("Player"))
-            this.numJumps = 0;
+        get { return 2; }
     }
 }
--- Assets/scripts/JumpController.cs
+++ Assets/scripts/JumpController.cs
@@ -2,34 +2,46 @@
 
 public class JumpController : MonoBehaviour
 {
-
+    
     public string jumpButtonName;
     public float jumpForce;
 
     private Rigidbody2D playerRB;
 
-    private bool canJump;
-    
+    protected virtual int AllowedJumps
+    {
+        get { return 1; }
+    }
+
+    private int numJumps;
+    private bool jumpPressed;
+
     protected virtual void Awake()
     {
         this.playerRB = this.GetComponent<Rigidbody2D>();
 
-        this.canJump = true;
+        this.numJumps = 0;
+        this.jumpPressed = false;
+    }
+
+    protected virtual void Update()
+    {
+        this.jumpPressed = Input.GetButtonDown(this.jumpButtonName);
     }
 
     protected virtual void FixedUpdate()
     {
-        if (Input.GetButtonDown(this.jumpButtonName) && this.canJump)
+        if (this.jumpPressed && this.numJumps < this.AllowedJumps)
         {
-            this.canJump = false;
+            this.numJumps++;
             this.playerRB.AddForce(jumpForce * Vector2.up * Time.deltaTime);
+            this.jumpPressed = false;
         }
-
     }
 
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Platform") || collision.CompareTag("Player"))
-            this.canJump = true;
+            this.numJumps = 0;
     }
 }
