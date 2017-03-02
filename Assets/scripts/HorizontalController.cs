d47338d6f725d61f3fd02fcb4d1599995a57ae51
kanirolf <wdomingo@uci.edu>
2017-03-01 21:32:50 -0800

    Move sprite orientation code from HorizontalController to SpriteOrienationController.

--- Assets/scripts/HorizontalController.cs
+++ Assets/scripts/HorizontalController.cs
@@ -9,7 +9,6 @@ public class HorizontalController : MonoBehaviour
     public float acceleration;
 
     protected Rigidbody2D playerRB;
-	protected bool right = true;
 
     protected virtual void Awake()
     {
@@ -19,25 +18,9 @@ public class HorizontalController : MonoBehaviour
     protected virtual void FixedUpdate()
     {
         float horizontal = Input.GetAxis(this.horizontalAxisName);
-		flip (horizontal);
         this.playerRB.AddForce(Vector2.right * this.acceleration * horizontal);
 
         if (Mathf.Abs(this.playerRB.velocity.x) >= this.maxSpeed)
-            this.playerRB.velocity = new Vector2(this.maxSpeed * horizontal, this.playerRB.velocity.y);
-        
+            this.playerRB.velocity = new Vector2(this.maxSpeed * horizontal, this.playerRB.velocity.y);   
     }
-	protected void flip (float horizontal){
-		if (horizontal < 0 && right == true) {
-			right = !right;
-			Vector3 scale = transform.localScale;
-			scale.x *= -1;
-			transform.localScale = scale;
-		}
-		if (horizontal > 0 && right == false) {
-			right = !right;
-			Vector3 scale = transform.localScale;
-			scale.x *= -1;
-			transform.localScale = scale;
-		}
-	}
 }
---
+++ Assets/scripts/SpriteOrientationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SpriteOrientationController : MonoBehaviour
+{
+    private Rigidbody2D entityRB;
+    private Transform entityTransform;
+
+    private short lastOrientation;
+
+    private void Awake()
+    {
+        this.entityRB = this.GetComponent<Rigidbody2D>();
+        this.entityTransform = this.GetComponent<Transform>();
+
+        this.lastOrientation = 1;
+    }
+
+    private void FixedUpdate()
+    {
+        short currentOrientation = (short)Mathf.Sign(this.entityRB.velocity.x);
+        if (currentOrientation != 0 && this.lastOrientation != currentOrientation)
+        {
+            this.lastOrientation = currentOrientation;
+            this.entityTransform.localScale = Vector3.Scale(this.entityTransform.localScale, new Vector3(-1, 1, 1));
+        }
+    }
+}
