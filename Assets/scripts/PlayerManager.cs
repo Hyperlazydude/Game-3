44c13289eb7603ee95e4d4dea39d32a90d70c745
kanirolf <wdomingo@uci.edu>
2017-03-02 00:14:47 -0800

    Add in test level scene.

---
+++ Assets/scripts/PlayerManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int playerNumber;
+
+    [HideInInspector]
+    public bool movementEnabled;
+
+    [HideInInspector]
+    public bool abilityEnabled;
+
+    public bool GetButtonDown(string button)
+    {
+        return Input.GetButtonDown(button + this.playerNumber);
+    }
+}
