597d08c299a9fdb856af11194702e07bb5965346
Hyperlazydude <AnimeAzn101@gmail.com>
2017-03-01 20:30:50 -0800

    Fixed shooting to shot in the other direction

--- Assets/scripts/ShootAbility.cs
+++ Assets/scripts/ShootAbility.cs
@@ -10,16 +10,20 @@ public class ShootAbility : MonoBehaviour {
 	public Transform point;
 
 	private float current;
-
+	private Rigidbody2D playerRB;
+	private HorizontalController player;
+	private Vector3 orignal;
+	private Vector3 rotate;
 	// Use this for initialization
 	void Awake () {
 		current = timeBetweenShots;
 		timeBetweenShots = 0;
+		player = GetComponent<HorizontalController> ();
+		playerRB = GetComponent<Rigidbody2D> ();
+		orignal = point.position;
 	}
 	void Start () {
-		
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.E)) {
