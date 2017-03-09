aee69b80b7f7e831a8f7880037279856622eb033
Hyperlazydude <AnimeAzn101@gmail.com>
2017-03-08 23:17:56 -0800

    Added Character Selection

---
+++ Assets/scripts/LoadLevel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+	public void startGame() {
+		SceneManager.LoadSceneAsync ("test-level");
+	}
+}
