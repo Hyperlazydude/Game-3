321c2ab2ef853cea3ee751dec5d19063611f8609
Hyperlazydude <AnimeAzn101@gmail.com>
2017-03-09 00:08:41 -0800

    Fixed GameManager

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,20 +6,26 @@ public class GameManager : MonoBehaviour {
 	public Player playerCharacter;
 	public GameObject playerPrefab;
 	public Camera mainCamera;
+	public GameObject countDown;
 
 	[HideInInspector]
 	public GameObject player1, player2;
 
 	// Use this for initialization
 	private void Awake () {
+		//Create Players
 		player1 = Instantiate (playerPrefab, new Vector3(-9,2.5f,0), playerPrefab.transform.rotation);
 		player2 = Instantiate (playerPrefab, new Vector3(9,2.5f,0), playerPrefab.transform.rotation);
+
+		//Set the player 2 controler
 		player2.GetComponent<PlayerManager> ().playerNumber = 2;
+
+		//Set the camera
 		CameraMovement movement = mainCamera.GetComponent<CameraMovement> ();
 		movement.player1 = player1.transform;
 		movement.player2 = player2.transform;
 
-
+		//Set the players' character
 		switch (playerCharacter.player1Name) {
 		case "Gunner":
 			player1.GetComponent<ShootAbility> ().enabled = true;
@@ -44,5 +50,10 @@ public class GameManager : MonoBehaviour {
 			player2.GetComponent<BombAbility> ().enabled = true;
 			break;
 		}
+
+		//Set up the Count Down
+		CountdownController counter = countDown.GetComponent<CountdownController>();
+		counter.player1Control = player1.GetComponent<HorizontalController> ();
+		counter.player2Control = player2.GetComponent<HorizontalController> ();
 	}
 }
