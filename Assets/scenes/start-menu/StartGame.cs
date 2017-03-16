e5a6aefa8a14844858a765a9c7ded5a27ccf6fd6
Hyperlazydude <AnimeAzn101@gmail.com>
2017-03-16 00:49:39 -0700

    Added Start Menu

---
+++ Assets/scenes/start-menu/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	public void start()
+	{
+		SceneManager.LoadSceneAsync("character-select");
+	}
+
+	public void exit()
+	{
+		Application.Quit ();
+	}
+}
